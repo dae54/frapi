4884e3f96e577b6254e4329580a5b27e612d6cc5
dae54 <amethysternest@gmail.com>
2020-10-22 18:02:47 +0300

    test auto running terminal scripts

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { exec } = require("child_process");
 // const { ReplSet } = require('mongodb-topology-manager')
 
 
@@ -23,16 +24,135 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_DB_HOST_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 module.exports = () => {
+
     // console.log('db init')
     var db = mongoose.connection
     db.on('error', (error) => {
-        console.log(error)
+        // exec("service mongod start", (error, stdout, stderr) => {
+        //     console.log(stdout)
+        // })
+        // console.log(error)
         console.log('connection error')
     })
     db.once('open', () => {
         console.log('db connected successfully')
     })
 }
+
+// const initMongoDB = async () => {
+//     console.log('initMongoDB started')
+//     mongoose.connect(process.env.MONGO_DB_HOST_LOCAL,
+//         { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+//     ).then(res => {
+//         console.log('initMongoDB then')
+//         console.log('db connected successfully')
+
+//         return res
+//         // var db = mongoose.connection
+//         // db.once('open', () => {
+//         //     console.log('db connected successfully')
+//         // })
+//     }).catch(error => {
+//         console.log('error catch')
+//         console.log(error)
+//         // return
+//         // tryReconnectingDB()
+//     })
+//     // var db = mongoose.connection
+//     // db.once('open', () => {
+//     //     console.log('db connected successfully')
+//     // })
+//     // db.on('error', (error) => {
+//     //     tryReconnectingDB()
+//     // })
+
+// }
+
+// // initMongoDB()
+
+// const tryReconnectingDB = () => {
+//     exec("service mongod start", (error, stdout, stderr) => {
+//         console.log(stdout)
+//         initMongoDB()
+//     })
+//     // console.log('connection error')
+// }
+
+// const checkConnection = async () => {
+//     await initMongoDB()
+//     console.log('check connection')
+
+//     var db = mongoose.connection
+//     // console.log(db)
+
+//     db.once('open', () => {
+//         console.log('db connected successfully')
+//         return
+//     })
+//     db.on('error', (error) => {
+//         console.log('error')
+//         tryReconnectingDB()
+//     })
+// }
+
+// module.exports = () => {
+//     // initMongoDB()
+//     checkConnection()
+// }
+    // var db = mongoose.connection
+    // initMongoDB().then(res => {
+    //     db.once('open', () => {
+    //         console.log('db connected successfully')
+    //     })
+    // }).catch(errA => {
+    //     console.log(errA)
+    //     exec("service mongod start", (errC, stdout, stderr) => {
+    //         if (errC) {
+    //             return console.log('error', errC)
+    //         }
+    //         console.log(stdout)
+
+    //         // initMongoDB().then(() => {
+    //         //     db.once('open', () => {
+    //         //         console.log('db connected successfully')
+    //         //     })
+    //         // })
+    //     })
+
+    //     // db.on('error', (errB) => {
+    //     //     exec("service mongod start", (errC, stdout, stderr) => {
+    //     //         if (errC) {
+    //     //             return console.log('error', errC)
+    //     //         }
+    //     //         initMongoDB().then(() => {
+    //     //             db.once('open', () => {
+    //     //                 console.log('db connected successfully')
+    //     //             })
+    //     //         })
+    //     //         console.log(stdout)
+    //     //         console.log('errC')
+    //     //         console.log(errC)
+    //     //     })
+    //     //     console.log('errB')
+    //     //     console.log(errB)
+
+    //     //     console.log('connection error')
+
+    //     //     console.log('errA')
+    //     //     console.log(errA)
+    //     // })
+    // })
+    // mongoose.connect(process.env.MONGO_DB_HOST_LOCAL,
+    //     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+    // ).then(res => {
+    //     console.log(res)
+    //     console.log('done connecting')
+    // }).catch(err => {
+    //     console.log(err)
+    //     console.log('error')
+    // })
+// }
+
 // module.exports = async () => {
 //     console.log('mongoose version ', mongoose.version)
 //     await setupReplicaSet()
