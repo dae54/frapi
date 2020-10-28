7131a3b6ed3316d006a58ad269f78fcb8c7b6700
dae54 <amethysternest@gmail.com>
2020-10-28 13:42:49 +0300

    add request aprove status

--- modules/Requests/requestAproves.model.js
+++ modules/Requests/requestAproves.model.js
@@ -7,11 +7,25 @@ let requestAproves = Schema({
         ref: 'users',
         required: true,
     },
-    requestId:{
+    requestId: {
         type: Schema.Types.ObjectId,
         ref: 'requests',
         required: true,
+    }, status: {
+        type: Number,
+        enum: [0, 1],
+        default: 0,
     }
-})
+},
+    { timestamps: true }
+)
+
+/**
+ * @param status
+ * 0 open
+ * 1 closed
+ * if open, means the aproved request is not yet aproved
+ * if closed, means the aproved request is aproved, ie minimum number of aproves is reached
+ */
 
 module.exports = mongoose.model('request_aproves', requestAproves)
